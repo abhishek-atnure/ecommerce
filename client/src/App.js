0c39b058e1fd85593fb6c5c3d8a95f08eea83584
Abhishek Atnure <atnureabhishek@gmail.com>
2021-05-09 22:50:17 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ function App() {
           }
         );
         const data = await response.json();
-        data === true ? dispatch(authenticated()) : dispatch(notAuthenticated);
+        data === true ? dispatch(authenticated()) : dispatch(notAuthenticated());
       } catch (error) {
         console.error(error.message);
       }
