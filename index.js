e94a0ad8da70810e0dbcefa46697b28147977a42
Abhishek Atnure <62664170+abhishek-atnure@users.noreply.github.com>
2021-06-06 16:30:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
-const timeout = require("connect-timeout");
+
 
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
@@ -14,9 +13,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-// app.use(timeout("10s"));
+
 app.use(express.json());
-app.use(morgan("dev"));
 app.use(cors());
 app.use(
   express.urlencoded({
